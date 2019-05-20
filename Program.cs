df76b2df8e825e01e10fc10f905315032d02e20e
parmodmaratha44@gmail.com <parmodmaratha44@gmail.com>
2019-05-20 13:40:25 +1200

    first Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello \n");
+            Console.WriteLine("welcome to c sharp...");
+        }
+    }
+}
